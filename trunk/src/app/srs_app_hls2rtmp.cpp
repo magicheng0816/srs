db2010bedcf6cca6c8f2d51f3d84891da2096386
magicheng0816 <magicheng0816@163.com>
2016-09-05 17:47:01 +0800

    test

--- trunk/src/app/srs_app_hls2rtmp.cpp
+++ trunk/src/app/srs_app_hls2rtmp.cpp
@@ -47,6 +47,7 @@ using namespace std;
 #include <srs_rtmp_amf0.hpp>
 #include <srs_raw_avc.hpp>
 #include <srs_app_http_conn.hpp>
+#include <srs_app_hls2rtmp.hpp>
 
 #if defined(SRS_AUTO_HTTP_CORE)
 
